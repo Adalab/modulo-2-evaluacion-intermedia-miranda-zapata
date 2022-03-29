02dceddc23d2405cffea00e77da1d6ef3aaf1fad
miranda-zapata <miranda.zapata@gmail.com>
2022-03-29 10:09:05 +0200

    js mandatory tasks done

--- js/main.js
+++ js/main.js
@@ -7,17 +7,32 @@ const playerBet = document.querySelector('.js-bet');
 const playerBalance = document.querySelector('.js-balance');
 const botMessage = document.querySelector('.js-message');
 const btnPlay = document.querySelector('.js-button');
+// Constante número aleatorio
+const randomNumber = getRandomNumber(6);
+console.log(randomNumber);
 
 // Función random
 function getRandomNumber(max) {
   return Math.ceil(Math.random() * max);
 }
 
-//
+// Función para adivinar el número
+function checkResults() {
+  const valueChoice = parseInt(playerChoice.value);
+
+  if (randomNumber === valueChoice) {
+    botMessage.innerHTML = 'Has ganado el doble de lo apostado';
+    balance = valueBet * 2 + balance;
+  } else {
+    botMessage.innerHTML = 'Has perdido lo apostado';
+    balance = balance - valueBet;
+  }
+}
 
 // Función manejadora
 function handleClickBtn(event) {
   event.preventDefault();
+  checkResults();
 }
 
 // Evento del botón
