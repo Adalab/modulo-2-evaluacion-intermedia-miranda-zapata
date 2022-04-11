7e5bb370c554c3162ecaf74c5a0c9281ac92eeec
miranda-zapata <miranda.zapata@gmail.com>
2022-04-11 10:11:35 +0200

    fix issue (some improvements)

--- js/main.js
+++ js/main.js
@@ -7,49 +7,69 @@ const playerChoice = document.querySelector('.js-number');
 const playerBet = document.querySelector('.js-bet');
 const playerBalance = document.querySelector('.js-balance');
 const botMessage = document.querySelector('.js-message');
+const botNumber = document.querySelector('.js-randomNumber');
 const btnPlay = document.querySelector('.js-button');
-// Constante número aleatorio (fuera de la función para que sólo se genere una vez y la usuaria pueda apostar siempre al mismo, hasta adivinarlo)
-const randomNumber = getRandomNumber(6);
-console.log(randomNumber);
 
 // Función random
 function getRandomNumber(max) {
   return Math.ceil(Math.random() * max);
 }
 
+// Función para asegurar que se rellenan los campos
+function validateInput() {
+  if (playerChoice.value === '' || playerBet.value === '') {
+    printMessage('Por favor, tira el dado y apuesta un dinerillo...');
+  } else {
+    checkResults();
+  }
+}
+
 // Función para adivinar el número
 function checkResults() {
-  // Aquí iría la constante randomNumber si quisiera generar un número aleatorio diferente cada jugada
+  const randomNumber = getRandomNumber(6);
+
   const valueChoice = parseInt(playerChoice.value);
   const valueBet = parseInt(playerBet.value);
 
   if (randomNumber === valueChoice) {
-    botMessage.innerHTML = 'Has ganado el doble de lo apostado';
+    printMessage('¡Has ganado el doble de lo apostado!');
     balance = valueBet * 2 + balance;
   } else {
-    botMessage.innerHTML = 'Has perdido lo apostado';
+    printMessage('Has perdido lo apostado');
     balance = balance - valueBet;
   }
+  printBotDie(`(El bot ha lanzado el dado y ha salido: ${randomNumber})`);
 }
 
 // Función para indicar el saldo
 function checkBalance() {
-  playerBalance.innerHTML = `¡Tu saldo es... ${balance}!`;
+  printBalance(`Tu saldo es... ${balance}`);
   return balance;
 }
 
 // Función para terminar el juego (limitando el saldo)
 function stopGame() {
   if (balance <= 0) {
-    botMessage.innerHTML = 'Ya has apostado suficiente por hoy...';
-    playerBalance.innerHTML = 'Has llegado a tu saldo mínimo: 0';
+    printMessage('Ya has apostado suficiente por hoy...');
+    printBalance('Has llegado a tu saldo mínimo: 0');
   } else if (balance >= 200) {
-    botMessage.innerHTML = 'Mejor lo dejamos aquí';
-    playerBalance.innerHTML = 'Has llegado a tu saldo tope: 200';
+    printMessage('Mejor lo dejamos aquí');
+    printBalance('Has llegado a tu saldo tope: 200');
   }
 }
 
-// Función para resetear el número aleatorio cuando la usuaria lo adivine (porque la constante está fuera de la función)
+// Funciones feedback painter
+function printMessage(message) {
+  botMessage.innerHTML = message;
+}
+
+function printBalance(money) {
+  playerBalance.innerHTML = money;
+}
+
+function printBotDie(die) {
+  botNumber.innerHTML = die;
+}
 
 // Función para resetear el juego
 /* function resetGame() {
@@ -61,7 +81,7 @@ function stopGame() {
 // Función manejadora
 function handleClickBtn(event) {
   event.preventDefault();
-  checkResults();
+  validateInput();
   checkBalance();
   stopGame();
 }
